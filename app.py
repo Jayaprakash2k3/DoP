b7ab01e7ad7758c54cdb52d73e0bb78843be3786
jayaprakash2k3 <jayaprakash2k3@gmail.com>
2023-07-21 11:45:46 +0530

    done

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 import dac 
+from pymongo import MongoClient
+from bson.json_util import dumps, loads
 app = Flask(__name__)
 CORS(app,origins=['*'])
 @app.route("/")
-def hello_world():
+def hello_world(request):
     param1 = float(request.args.get('lat'))
     param2 = float(request.args.get('lng'))
     return {"dac":dac.encode(param1, param2)}
+
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['DAC']
+@app.route('/DAC')
+def DAC():
+    try:
+        users_collection = db['Polygon']
+        polygons = list(users_collection.find())
+        return dumps(polygons)
+        # return "Hello World"
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- index.js
+++ index.js
@@ -3,16 +3,66 @@ async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
     const map = new Map(document.getElementById("map"), {
-      center: { lat:12.967558013562913, lng:77.5622669834594  },
-      zoom: 14,
+      center: { lat:12.98581214, lng:77.55544436 },
+      zoom: 17,
       mapId: "4504f8b37365c3d0",
     });
-    map.data.loadGeoJson("demo.json");
-
-    map.data.setStyle({
-        fillColor: "red",
+    // map.data.loadGeoJson("demo.json");
+   
+    // const cords =[]
+  var prev = null;
+  fetch("http://localhost:5000/DAC").then((response) => {
+    return response.json();
+  }).then((data) => {
+    data.forEach((element) => {
+      const cords = []
+      element.geometry.coordinates.forEach((cord) => {
+        cord.forEach((cord) => {
+          cords.push({ lat: cord[1], lng: cord[0] })
+        })
+      })
+      
+      const curr = new google.maps.Polygon({
+        paths: cords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0,
         strokeWeight: 2,
-    });
+        fillColor: "#FF0000",
+        fillOpacity: 0,
+        dac: element.properties.name,
+        lat: element.properties.lat,
+        lng: element.properties.lng
+      });
+      curr.setMap(map);
+      const priceTag = document.createElement("div");
+      priceTag.className = "price-tag";
+      priceTag.textContent = curr.dac;
+      const marker = new AdvancedMarkerElement({
+        position: { lat: curr.lat, lng: curr.lng },
+        map: null,
+        content: priceTag // Use a property value as the marker title
+      });
+      document.getElementById("showDAC").addEventListener("change", (event) => {
+        marker.map = event.target.checked ? map : null;
+      });
+      document.getElementById("showBounding").addEventListener("change", (event) => {
+        event.target.checked? curr.setOptions({ fillOpacity: 0.5, strokeOpacity: 0.5, editable: false, draggable: false }):curr.setOptions({ fillOpacity: 0, strokeOpacity: 0, editable: false, draggable: false });
+        
+      })
+      curr.addListener("click", (event) => {
+        if (prev != null) {
+          prev.setOptions({ fillOpacity: 0, strokeOpacity: 0, editable: false, draggable: false });
+          curr.setOptions({ fillOpacity: 0.5, strokeOpacity: 0.5, editable: true, draggable: true });
+        } else {
+          curr.setOptions({ fillOpacity: 0.5, strokeOpacity: 0.5, editable: true, draggable: true });
+        }
+        prev = curr;
+      })
+
+      // Construct the polygon.
+    })
+  });
+    // console.log(cords);
 
     map.data.addListener("addfeature", (event) => {
         const feature = event.feature;
@@ -28,15 +78,17 @@ async function initMap() {
           map: null,
           content: priceTag // Use a property value as the marker title
         });
-        
-    map.addListener("zoom_changed", (event) => {
+        document.getElementById("showDAC").addEventListener("change", (event) => {
+          marker.map = event.target.checked? map : null;
+        });
+    // map.addListener("zoom_changed", (event) => {
 
-        const zoom = map.getZoom();
-        if (zoom) {
-          // Only show each marker above a certain zoom level.
-          marker.map = zoom > 19 ? map : null;
-        }
-      });
+    //     const zoom = map.getZoom();
+    //     if (zoom) {
+    //       // Only show each marker above a certain zoom level.
+    //       marker.map = zoom > 19 ? map : null;
+    //     }
+    //   });
       });
     map.data.addListener("click", (event) => {
         const payload = { lat:event.latLng.lat(), lng: event.latLng.lng()  };
@@ -76,15 +128,13 @@ async function initMap() {
               position: {lat:lat,lng:lng},
               map: map,
               content: priceTag // Use a property value as the marker title
-            });
+             });
+            
            })   
         
+           
     })
 
-    
-       
-    
-    
     }
   
   initMap();
